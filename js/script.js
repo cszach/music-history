1a4db90631787f6bc65840471d85fd69faeb5317
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-11-17 22:56:13 +0700

    Update script.js
    New implementation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,54 @@
+/**
+ * Function to check if the system for this presentation is valid.
+ * Return 0 if current device is desktop and the presentation is being viewed in Google Chrome,
+ * which can have more animations.
+ * Return 1 if current device is iPad and the presentation is being viewed in Safari,
+ * in which some of the presentation's animations are limited.
+ * Return -1 if the current system is not at all valid.
+ * 
+ * @return {integer} An indicator integer
+ */
+function validSystem() {
+    if (is.chrome() && is.desktop()) {
+        return 0;
+    }
+
+    if (is.ipad() && is.safari()) {
+        return 1;
+    }
+
+    return -1;
+}
+
+/**
+ * All main activities of the presentation
+ */
 function main() {
-    if (is.chrome()) {
-        $("#_02").children("h2").hover(
-            function() { 
-                $(this).css("padding-right", "10rem");
-            },
-            function() {
-                $(this).css("padding-right", "initial");
-            }
-        );
+    switch (validSystem()) {
+        case 0:
+            $("#_02").children("h2").hover(
+                function() { 
+                    $(this).css("padding-right", "10rem");
+                },
+                function() {
+                    $(this).css("padding-right", "initial");
+                }
+            );
+            break;
+        case 1:
+            break;
+        case -1:
+            alertMsg = "Your browser and/or device is not supported.\n"
+                + "If you are on a desktop device, view the presentation in Google Chrome or a Chrome-based web browser like Chromiun.\n"
+                + "If you are on an iPad, view this in Safari.\nYou can proceed, however, you might see unexpected things";
+            alert(alertMsg);
+            delete alertMsg;
+            break;
+        default:
+            location.reload();
     }
-    else if (!is.safari() || !is.ipad()) {
-        alert("Browser or device not supported.");
-    }
+
+    // Shared JavaScript actions
 }
 
 $(document).ready(main);
